25185484efe1cdba1039006f1550824c5211fc3d
krs <kristianrorensvanholm@gmail.com>
2024-02-13 16:54:50 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h1>Drivstoff kalkulator</h1>
+        <img src="https://cdn-icons-png.freepik.com/512/3815/3815878.png"
+            width="50"
+            style="margin-left:20px"
+        />
+      </div>
+    </v-app-bar>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,119 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <v-card id="calculator">
+        <v-radio-group v-model="radios">
+          <v-radio label="Volum" value="liter"></v-radio>
+          <v-radio label="Vekt" value="kilo"></v-radio>
+        </v-radio-group><br/>
+        <v-slider
+            :label="radios"
+            v-model="slider"
+            :max="sliderLimit"
+            :min="1"
+            thumb-label="always"
+            step="1"
+        >
+        </v-slider>
+    <div v-if="radios=='liter'" class="container">
+        <v-card class="fuel" width="350px" outlined shaped v-for="(item, key) in fuels" :key=key>
+            <h1>{{item.name}} </h1>
+            Energi: {{Math.round(item.byVolume * slider)}} kWh <br/>
+            Masse: {{Math.round((item.byVolume * slider)/item.byMass)}} kg <br/>
+        </v-card>
+    </div>
+    <div v-else class="container">
+        <v-card class="fuel" width="350px" outlined shaped v-for="(item, key) in fuels" :key=key>
+            <h1>{{item.name}} </h1>
+            Energi: {{Math.round(item.byMass * slider)}} kWh <br/>
+            Volum: {{Math.round((item.byMass * slider)/item.byVolume)}} liter <br/>
+        </v-card>
+    </div>
+    <br/>
+    Kilder<br/>
+    <a href="https://everything2.com/index.pl?node=energy%20density">everything2 11/10/2004</a><br/>
+    <a href="https://www.idealhy.eu/index.php?page=lh2_outline">idealhy ~2013</a><br/>
+    <a href="https://www.arenaev.com/how_big_is_the_fuel_tank_of_an_ev-news-139.php">arenaev 10.4.22</a>
+    <a href="http://www.h2data.de/">h2Data.de</a><br/><br/>
+    Notater<br/>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+        <li>Hydrogen i væske form krever ~36% av sin egen energi for kjøling.</li>
+        <li>Li-Ion tallene er basert på Tesla Model S.</li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    </v-card>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+        radios: "liter",
+        slider: 0,
+        volumeUnit: "kWh/l",
+        massUnit: "kWH/Kg",
+        fuels: [
+            {
+                name: "Hydrogen",
+                byVolume: 0.003,
+                byMass: 33.33,
+            },
+            {
+                name: "Hydrogen (700 bar)",
+                byVolume: 1.4,
+                byMass: 33.33
+            },
+            {
+                name: "Hydrogen (-252.9 °C)",
+                byVolume: 2.3,
+                byMass: 33.33
+            },
+            {
+                name: "Diesel",
+                byVolume: 10,
+                byMass: 11.9   
+            },
+            {
+                name: "Bensin",
+                byVolume: 8.8,
+                byMass: 12,
+            },
+            {
+                name: "Lithium-Ion batteri",
+                byVolume: 0.6, 
+                byMass: 0.15,
+            },
+        ]
+    }),
+      computed: {
+        sliderLimit: function() {
+            if (this.radios == "liter") {
+                return 200
+            }
+            return 600
+        }
+      },
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+
+#calculator {
+    min-width: 400px;
+    max-width: 1200px;
+    margin: auto;
+    width: 65%;
+    padding: 10px;
+    margin-top: 50px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.fuel {
+    min-width: 350px;
+    margin: 10px;
+    padding: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.container {
+    justify-content: space-around;
+    display:flex;
+    flex-wrap:wrap;
 }
 </style>
