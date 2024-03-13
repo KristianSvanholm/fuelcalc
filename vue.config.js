0f8b12e8536fd20d3a20641c8175c426016a4e9b
kristian <kristianrorensvanholm@gmail.com>
2024-03-13 11:31:02 +0100

    update dist

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-    publicPath: "/fuelcalc/",
+//const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+publicPath: "/fuelcalc/",
   transpileDependencies: [
     'vuetify'
   ]
-})
+}
