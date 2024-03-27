8875b161a9e846293a48263124fbdb362c50b947
kristian <kristianrorensvanholm@gmail.com>
2024-03-27 11:30:35 +0100

    total co2 per fuel type

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,6 +18,7 @@
             <h1>{{item.name}} </h1>
             Energi: {{Math.round(item.byVolume * slider)}} kWh <br/>
             Masse: {{Math.round((item.byVolume * slider)/item.byMass)}} kg <br/>
+            Utslipp: {{Math.round(item.co2l * slider)}} kg <br/>
         </v-card>
     </div>
     <div v-else class="container">
@@ -25,6 +26,7 @@
             <h1>{{item.name}} </h1>
             Energi: {{Math.round(item.byMass * slider)}} kWh <br/>
             Volum: {{Math.round((item.byMass * slider)/item.byVolume)}} liter <br/>
+            Utslipp: {{Math.round((item.byMass * slider)/item.byVolume*item.co2l)}}<br/>
         </v-card>
     </div>
     <br/>
@@ -32,6 +34,7 @@
     <a href="https://everything2.com/index.pl?node=energy%20density">everything2 11/10/2004</a><br/>
     <a href="https://www.idealhy.eu/index.php?page=lh2_outline">idealhy ~2013</a><br/>
     <a href="https://www.arenaev.com/how_big_is_the_fuel_tank_of_an_ev-news-139.php">arenaev 10.4.22</a><br/>
+    <a href="https://natural-resources.canada.ca/sites/www.nrcan.gc.ca/files/oee/pdf/transportation/fuel-efficient-technologies/autosmart_factsheet_9_e.pdf">canadian govt 2014</a><br/>
     <a href="http://www.h2data.de/">h2Data.de</a><br/><br/>
     Notater<br/>
     <ul>
@@ -55,31 +58,37 @@
                 name: "Hydrogen",
                 byVolume: 0.003,
                 byMass: 33.33,
+                co2l: 0,
             },
             {
                 name: "Hydrogen (700 bar)",
                 byVolume: 1.4,
-                byMass: 33.33
+                byMass: 33.33,
+                co2l: 0,
             },
             {
                 name: "Hydrogen (-252.9 °C)",
                 byVolume: 2.3,
-                byMass: 33.33
+                byMass: 33.33,
+                co2l: 0,
             },
             {
                 name: "Diesel",
                 byVolume: 10,
-                byMass: 11.9   
+                byMass: 11.9,
+                co2l: 2.7,
             },
             {
                 name: "Bensin",
                 byVolume: 8.8,
                 byMass: 12,
+                co2l: 2.3,
             },
             {
                 name: "Lithium-Ion batteri",
                 byVolume: 0.6, 
                 byMass: 0.15,
+                co2l: 0,
             },
         ]
     }),
