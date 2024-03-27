9577823ff52b687baa29ba66c5b1fc6a26d22f53
kristian <kristianrorensvanholm@gmail.com>
2024-03-27 12:48:47 +0100

    Add fuel prices

--- src/components/FuelCalc.vue
+++ src/components/FuelCalc.vue
@@ -19,6 +19,7 @@
             Energi: {{Math.round(item.byVolume * slider)}} kWh <br/>
             Masse: {{Math.round((item.byVolume * slider)/item.byMass)}} kg <br/>
             Utslipp: {{Math.round(item.co2l * slider)}} kg <br/>
+            Pris: {{Math.round((item.byVolume * slider)/item.byMass * item.pricekg)}} € <br/>
         </v-card>
     </div>
     <div v-else class="container">
@@ -27,6 +28,7 @@
             Energi: {{Math.round(item.byMass * slider)}} kWh <br/>
             Volum: {{Math.round((item.byMass * slider)/item.byVolume)}} liter <br/>
             Utslipp: {{Math.round((item.byMass * slider)/item.byVolume*item.co2l)}} kg <br/>
+            Pris: {{Math.round(slider * item.pricekg)}} € <br/>
         </v-card>
     </div>
     <br/>
@@ -35,6 +37,8 @@
     <a href="https://www.idealhy.eu/index.php?page=lh2_outline">idealhy ~2013</a><br/>
     <a href="https://www.arenaev.com/how_big_is_the_fuel_tank_of_an_ev-news-139.php">arenaev 10.4.22</a><br/>
     <a href="https://natural-resources.canada.ca/sites/www.nrcan.gc.ca/files/oee/pdf/transportation/fuel-efficient-technologies/autosmart_factsheet_9_e.pdf">canadian govt 2014</a><br/>
+    <a href="https://h2.live/en/">H2 kg price h2.live</a><br/>
+    <a href="https://www.cargopedia.net/europe-fuel-prices">diesel and gas price cargopedia.net 25.03.24</a><br/>
     <a href="http://www.h2data.de/">h2Data.de</a><br/><br/>
     Notater<br/>
     <ul>
@@ -59,36 +63,42 @@
                 byVolume: 0.003,
                 byMass: 33.33,
                 co2l: 0,
+                pricekg: 15.75,
             },
             {
                 name: "Hydrogen (700 bar)",
                 byVolume: 1.4,
                 byMass: 33.33,
                 co2l: 0,
+                pricekg: 15.75,
             },
             {
                 name: "Hydrogen (-252.9 °C)",
                 byVolume: 2.3,
                 byMass: 33.33,
                 co2l: 0,
+                pricekg: 15.75,
             },
             {
                 name: "Diesel",
                 byVolume: 10,
                 byMass: 11.9,
                 co2l: 2.7,
+                pricekg: 1.505,
             },
             {
                 name: "Bensin",
                 byVolume: 8.8,
                 byMass: 12,
                 co2l: 2.3,
+                pricekg: 1.581,
             },
             {
                 name: "Lithium-Ion batteri",
                 byVolume: 0.6, 
                 byMass: 0.15,
                 co2l: 0,
+                pricekg: 0, //TODO
             },
         ]
     }),
