ac91ed4ffaa3436d5dca9a56029cd63bd456b72e
krs <kristianrorensvanholm@gmail.com>
2024-04-14 17:29:37 +0200

    Rework

--- src/components/FuelCalc.vue
+++ src/components/FuelCalc.vue
@@ -1,45 +1,65 @@
 <template>
     <v-card id="calculator">
-        <v-radio-group v-model="radios">
-          <v-radio label="Volum" value="liter"></v-radio>
-          <v-radio label="Vekt" value="kilo"></v-radio>
-        </v-radio-group><br/>
-        <v-slider
-            :label="radios"
-            v-model="slider"
-            :max="sliderLimit"
-            :min="1"
-            thumb-label="always"
-            step="1"
-        >
-        </v-slider>
-    <div v-if="radios=='liter'" class="container">
-        <v-card class="fuel" width="350px" outlined shaped v-for="(item, key) in fuels" :key=key>
-            <h1>{{item.name}} </h1>
-            Energi: {{Math.round(item.byVolume * slider)}} kWh <br/>
-            Masse: {{decimals((item.byVolume * slider)/item.byMass)}} kg <br/>
-            Utslipp: {{decimals(item.co2l * slider)}} kg <br/>
-            Pris: {{decimals((item.byVolume * slider)/item.byMass * item.pricekg)}} kr <br/>
-            Pris / kwh: {{decimals(item.pricekg / item.byMass)}} kr 
-         </v-card>
-    </div>
-    <div v-else class="container">
-        <v-card class="fuel" width="350px" outlined shaped v-for="(item, key) in fuels" :key=key>
-            <h1>{{item.name}} </h1>
-            Energi: {{decimals(item.byMass * slider)}} kWh <br/>
-            Volum: {{decimals((item.byMass * slider)/item.byVolume)}} liter <br/>
-            Utslipp: {{decimals((item.byMass * slider)/item.byVolume*item.co2l)}} kg <br/>
-            Pris: {{decimals(slider * item.pricekg)}} kr <br/>
-            Pris / kwh: {{decimals(item.pricekg / item.byMass)}} kr
+
+    <v-slider
+        label="km"
+        v-model="dist"
+        :max="1000"
+        :min="0"
+        thumb-label="always"
+        step="10"
+    />
+
+    <div class="container"> 
+        <v-card class="fuel" width="350px" outlined shaped>
+            <h1> Hydrogen (700 bar) </h1>
+            tank: {{decimals(hydrogen.kgpkm * dist)}} kg <br/>
+            Pris: {{decimals(hydrogen.pricekg * hydrogen.kgpkm * dist)}} kr <br/> 
+            Utslipp: {{decimals(0)}} kg Co2 <br/>
+            Energi: {{decimals(hydrogen.kwhpkg * hydrogen.kgpkm * dist)}} kWh <br/>
+            <br/>
+            <v-row>
+                <v-col><v-text-field label="kr / kg" variant="underlined" v-model="hydrogen.pricekg" type="number"/></v-col>
+                <v-col><v-text-field label="kg / km" variant="underlined" v-model="hydrogen.kgpkm" type="number"/></v-col>
+            </v-row>
+
+        </v-card>
+
+        <v-card class="fuel" width="350px" outlined shaped>
+            <h1> Diesel </h1>
+            tank: {{decimals(diesel.lpkm * dist)}} l <br/>
+            Pris: {{decimals(diesel.pricel * diesel.lpkm * dist)}} kr <br/> 
+            Utslipp: {{decimals(diesel.co2l * diesel.lpkm * dist)}} kg Co2 <br/>
+            Energi: {{decimals(diesel.kwhpl * diesel.lpkm * dist)}} kWh <br/>
+            <br/>
+            <v-row>
+                <v-col><v-text-field label="kr / liter" variant="underlined" v-model="diesel.pricel" type="number"/></v-col>
+                <v-col><v-text-field label="liter / km" variant="underlined" v-model="diesel.lpkm" type="number"/></v-col>
+            </v-row>
+        </v-card>
+
+        <v-card class="fuel" width="350px" outlined shaped>
+            <h1> Bensin </h1>
+            tank: {{decimals(bensin.lpkm * dist)}} l <br/>
+            Pris: {{decimals(bensin.pricel * bensin.lpkm * dist)}} kr <br/> 
+            Utslipp: {{decimals(bensin.co2l * bensin.lpkm * dist)}} kg Co2 <br/>
+            Energi: {{decimals(bensin.kwhpl * bensin.lpkm * dist)}} kWh <br/>
+            <br/> 
+            <v-row>
+                <v-col><v-text-field label="kr / liter" variant="underlined" v-model="bensin.pricel" type="number"/></v-col>
+                <v-col><v-text-field label="liter / km" variant="underlined" v-model="bensin.lpkm" type="number"/></v-col>
+            </v-row>
         </v-card>
     </div>
+
     <br/>
     Kilder<br/>
     <a href="https://www.idealhy.eu/index.php?page=lh2_outline">idealhy ~2013</a><br/>
     <a href="https://natural-resources.canada.ca/sites/www.nrcan.gc.ca/files/oee/pdf/transportation/fuel-efficient-technologies/autosmart_factsheet_9_e.pdf">canadian govt 2014</a><br/>
     <a href="https://h2.live/en/">H2 kg price h2.live</a><br/>
     <a href="https://www.cargopedia.net/europe-fuel-prices">diesel and gas price cargopedia.net 25.03.24</a><br/>
-    <a href="http://www.h2data.de/">h2Data.de</a><br/><br/>
+    <a href="http://www.h2data.de/">h2Data.de</a><br/>
+    <a href="https://www.nrel.gov/docs/fy19osti/73010.pdf">nrel gov - h2 fuel efficiency 14.04.14</a><br/><br/>
     Notater<br/>
     <ul>
         <li>Hydrogen i væske form krever ~36% av sin egen energi for kjøling.</li>
@@ -53,60 +73,33 @@
     name: 'FuelCalc',
 
     data: () => ({
-        radios: "liter",
-        slider: 0,
-        volumeUnit: "kWh/l",
-        massUnit: "kWH/Kg",
-        fuels: [
-            {
-                name: "Hydrogen",
-                byVolume: 0.003,
-                byMass: 33.33,
-                co2l: 0,
-                pricekg: 300,
-            },
-            {
-                name: "Hydrogen (700 bar)",
-                byVolume: 1.4,
-                byMass: 33.33,
-                co2l: 0,
-                pricekg: 300,
-            },
-            {
-                name: "Hydrogen (-252.9 °C)",
-                byVolume: 2.3,
-                byMass: 33.33,
-                co2l: 0,
-                pricekg: 300,
-            },
-            {
-                name: "Diesel",
-                byVolume: 10,
-                byMass: 11.9,
-                co2l: 2.7,
-                pricekg: 23.80,
-            },
-            {
-                name: "Bensin",
-                byVolume: 8.8,
-                byMass: 12,
-                co2l: 2.3,
-                pricekg: 28.64,
-            }
-        ]
+        dist: 500,
+        hydrogen: {
+            name: "Hydrogen",
+            kwhpkg: 33.33,
+            pricekg: 300,
+            kgpkm: 0.012,
+        },
+        diesel: {
+            name: "Diesel",
+            kwhpl: 10,
+            co2l: 2.7,
+            pricel: 22,
+            kmpl: 20.92,
+            lpkm: 0.06,
+        },
+        bensin : {
+            name: "Bensin",
+            kwhpl: 8.8,
+            co2l: 2.3,
+            pricel: 24,
+            lpkm: 0.07,
+        },
     }),
       methods: {
         decimals(num){
             return Math.round(num*100) / 100
         },
-      },
-      computed: {
-        sliderLimit: function() {
-            if (this.radios == "liter") {
-                return 200
-            }
-            return 600
-        }
       },
   }
 </script>
@@ -133,4 +126,5 @@
     display:flex;
     flex-wrap:wrap;
 }
+
 </style>
